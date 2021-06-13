ef42e95aebbd6f930150bf407c48ff52c624af30
lemamon <demmarinho@gmail.com>
2021-06-12 23:11:29 -0300

    feat: created app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Typewriter from "typewriter-effect";
+
+import logo from "./logo.svg";
+
+const date = new Date();
 
 function App() {
+  const [fact, setFact] = useState({
+    text: "",
+  });
+  const today = `${date.getUTCMonth() + 1}/${date.getDate()}`;
+  const getRamdomFact4Today = async () => {
+    const data = await fetch(`http://numbersapi.com/${today}/date?json`).then(
+      (response) => response.json()
+    );
+    setFact(data);
+  };
+
+  const getRamdomFactCallback = useCallback(getRamdomFact4Today, [
+    getRamdomFact4Today,
+  ]);
+
+  useEffect(() => {
+    getRamdomFactCallback(); // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={logo} alt="calendar logo" className="logo" />
+        <h2>Today's in {fact.year} </h2>
+
+        <div className="text">
+          <Typewriter
+            options={{
+              strings: fact.text,
+              autoStart: true,
+              delay: 80,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
